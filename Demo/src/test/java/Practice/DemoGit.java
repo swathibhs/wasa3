b5f10307f5e290915d1eb41d3874c42999d62345
swathibhs <121571278+swathibhs@users.noreply.github.com>
2023-03-29 13:13:59 +0530

    Add files via upload

---
+++ Demo/src/test/java/Practice/DemoGit.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class DemoGit {
+
+	@Test
+	public void sample()
+	{
+		System.out.println("welcome");
+        System.out.println("lishi");
+}
+}
